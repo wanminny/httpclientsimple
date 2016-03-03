6bd690cd624a8b96e7bacaa74a17faa6cdc8568a
wanmin <min.wan@yoho.cn>
2016-03-03 12:01:44 +0800

    Initial Commit

---
+++ HttpClient/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HttpClient
+//
+//  Created by wan on 16/3/3.
+//  Copyright © 2016年 wan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HttpClient/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HttpClient
+//
+//  Created by wan on 16/3/3.
+//  Copyright © 2016年 wan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
